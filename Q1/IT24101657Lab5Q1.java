9232dc708075ce773c7cc0657ab306d0bc7ccddb
IT24101657 <it24101657@my.sliit.lk>
2024-08-21 09:18:28 +0530

    IT24101657Lab5Q1.java

---
+++ Q1/IT24101657Lab5Q1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class IT24101657Lab5Q1{
+public static void main(String[]args){
+	Scanner input=new Scanner(System.in);
+	int num1,num2,num3,min,max;
+	System.out.print("Enter the first integer:");
+	num1=input.nextInt();
+	System.out.print("Enter the second integer:");
+	num2=input.nextInt();
+	System.out.print("Enter the third integer:");
+	num3=input.nextInt();
+
+	System.out.print("User entered numbers are:"+num1);
+	System.out.print(" "+num2);
+	System.out.print(" "+num3+"\n");
+    	if(num1>num2){
+           max=num1;
+	   min=num2;
+	}
+	else{
+	   max=num2;
+	   min=num1;
+	}
+	if (num3>max){
+	   max=num3;}
+	if(num3<min){
+	   min=num3;}
+	
+	System.out.println("The Smallest number is:"+min);
+	System.out.println("The Largest number is:"+max);
+
+	}
+}
